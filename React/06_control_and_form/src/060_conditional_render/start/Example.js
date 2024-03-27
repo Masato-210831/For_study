c868e13daf7ada0af28b9f1b1b5b775f80c65d12
masato-210831 <b86500b@gmail.com>
2024-03-27 20:11:44 +0900

    React study in 2024-03-27

--- React/06_control_and_form/src/060_conditional_render/start/Example.js
+++ React/06_control_and_form/src/060_conditional_render/start/Example.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  // const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,13 +16,23 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? "";
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+            // if (animal === "Dog") {
+            //   return <li key={animal}>{animal}★</li>;
+            // } else {
+            //   return <li key={animal}>{animal}</li>;
+            // }
+
+            // 三項演算子
+            // return <li key={animal}>{animal === "Dog" ? animal + "★" : animal}</li>;
+
+            // && => Trueの時に何か表示したい場合
+            return <li key={animal}>{animal}{animal==='Dog' && '★'}</li>;
+          })}
       </ul>
     </>
   );
