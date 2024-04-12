cc5c44c41f35ef184196c14481a128c642ec9b86
masato-210831 <b86500b@gmail.com>
2024-04-12 22:55:50 +0900

    React study in 2024-04-12

--- React/11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ React/11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,42 @@
+import { useState, useReducer } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((preve, { type }) => {
+    switch (type) {
+      case "+":
+        return ++preve;
+      case "-":
+        return --preve;
+      default:
+        throw new Error("不明なactionです。");
+    }
+    // if(action === '+'){
+    //   return ++preve
+    // } else if(action === '-'){
+    //   return --preve
+    // }
+  }, 0);
+
+  const countUp = () => {
+    setState((preve) => ++preve);
+  };
+
+  const rcountp = () => {
+    dispatch({ type: "+" });
+  };
+
+  const rcountDown = () => {
+    dispatch({ type: "-" });
+  };
+
   return (
     <>
+      <h3>{state}</h3>
+      <button onClick={countUp}>+</button>
+      <h3>{rstate}</h3>
+      <button onClick={rcountp}>+</button>
+      <button onClick={rcountDown}>-</button>
     </>
   );
 };
