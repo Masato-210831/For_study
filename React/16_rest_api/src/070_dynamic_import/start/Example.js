6b5a3e79e960b3bae1e4b3a604582e42289ee82b
masato-210831 <b86500b@gmail.com>
2024-05-14 09:46:13 +0900

    React study for dynamic import, get started Next.js in 2024-05-14

--- React/16_rest_api/src/070_dynamic_import/start/Example.js
+++ React/16_rest_api/src/070_dynamic_import/start/Example.js
@@ -1,4 +1,13 @@
+import {add} from './add'
+
+
 const Example = () => {
+  const dynamicImport = async (a , b) => {
+    const module = await import ('./add')
+    console.log( module.add(a, b))
+  }
+  dynamicImport(1, 2)
+
   
 };
 
--- React/16_rest_api/src/080_react_lazy/start/components/ComponentA.js
+++ React/16_rest_api/src/080_react_lazy/start/components/ComponentA.js
@@ -1,5 +1,8 @@
 const ComponentA = () => {
+  console.log('loaded componentA')
   return <h3>Loaded ComponentA!</h3>;
 };
+
+
 
 export default ComponentA;
